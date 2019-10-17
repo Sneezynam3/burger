0b550fff34e2b11a3ebf5acd133aa312553be0af
James Snell <Vulpes3.1415@yahoo.com>
2019-10-17 16:45:08 -0600

    updated routes and handlebars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
@@ -26,4 +26,5 @@ app.use(routes);
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
+
 });
