7d949589e4f75247295066716825834ec3b71e4b
James Snell <Vulpes3.1415@yahoo.com>
2019-10-17 09:27:59 -0600

    Inserted burgers into table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (Id, burger_name, devoured) VALUES (id, 'Pineneedle-palmleaf burger', false);
+INSERT INTO burgers (Id, burger_name, devoured) VALUES (id, 'Veggie burger', false);
+INSERT INTO burgers (Id, burger_name, devoured) VALUES (id, 'Cheesy Ranch burger', false);
