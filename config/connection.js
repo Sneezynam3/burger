87a06205e67385bb37fc8879dcfa623a15e9213a
James Snell <Vulpes3.1415@yahoo.com>
2019-10-19 16:05:19 -0600

    refresh

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.argv.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Rams2014!',
+    database: 'burgers_db'
+  })
+};
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,7 +20,6 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
