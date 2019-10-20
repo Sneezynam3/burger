a152267195d63f648043dc79bfbc45c559302c41
James Snell <Vulpes3.1415@yahoo.com>
2019-10-20 12:19:23 -0600

    heroku testing

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.argv.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.argv.JAWSDB_URL){
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Rams2014!',
-    database: 'burgers_db'
+    host: 'thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'gtfwwy4cbu5cdr7f',
+    password: 'xz4w7c6xe1cog2xr',
+    database: 'wunbnxy5znk75ssu'
   })
-};
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Rams2014!',
+//     database: 'burgers_db'
+//   })
+// };
 
 connection.connect(function(err) {
   if (err) {
